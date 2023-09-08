c8cb87e8ecd2e3ed6ac27cd9f9ea32cf1ee4d3af
Juampibruera <136599420+Juampibruera@users.noreply.github.com>
2023-09-08 18:30:08 -0300

    Update app.py
    Signed-off-by: Juampibruera <136599420+Juampibruera@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# Trabajo Práctico I - Programación II
-
-
 import os
+import biblioteca
 
 print("Bienvenido!")
 respuesta = ''
@@ -10,36 +8,32 @@ def menu():
     print("1 - Gestionar Prestamo")
     print("2 - Gestionar Devolucion")
     print("3 - Registrar nuevo libro")
-    print("4 - Elimiar ejemplar")
-    print("5 - Mostrar ejemplares perstados")
+    print("4 - Eliminar ejemplar")
+    print("5 - Mostrar ejemplares prestados")
     print("6 - Salir")
 
 while respuesta != "salir":
     menu()
     opt = input("\n Ingrese la opción de menú: ")
-    os.system ("cls") #Limpiar pantalla
+    os.system("cls")  # Limpiar pantalla
     if opt.isnumeric():
-        if int(opt) == 1:
-            ejemplares_prestados()
-            print(ejemplares_prestados)
-        elif int(opt) == 2:
-            devolver_ejemplar_libro()
-            print(devolver_ejemplar_libro)
-        elif int(opt) == 3:
-            registrar_nuevo_libro()
-            print(registrar_nuevo_libro)
-        elif int(opt) == 4:
-            eliminar_ejemplar_libro()
-            print(eliminar_ejemplar_libro)
-        elif int(opt) == 5:
-            ejemplares_prestados()
-            print(ejemplares_prestados)
-        elif int(opt) == 6:
+        if opt == "1":
+            biblioteca.ejemplares_prestados()
+        elif opt == "2":
+            biblioteca.devolver_ejemplar_libro()
+        elif opt == "3":
+            biblioteca.registrar_nuevo_libro()
+        elif opt == "4":
+            biblioteca.eliminar_ejemplar_libro()
+        elif opt == "5":
+            biblioteca.mostrar_ejemplares_prestados()  # Supongo que esta función existe en tu módulo biblioteca
+        elif opt == "6":
             respuesta = "salir"
-        else: print("Ingrese una opción válida")
-    else: 
+        else:
+            print("Ingrese una opción válida")
+    else:
         print("Ingrese una opción numérica")
 
-    input("Presione cualquier tecla para continuar....") # Pausa
+    input("Presione cualquier tecla para continuar....")  # Pausa
 
-print("Hasta luego!."
+print("Hasta luego!.")
